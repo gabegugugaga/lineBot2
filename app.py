83d47538c6161ac5a5aeb51b374d413b7663f1e4
gabegugugaga <72595808+gabegugugaga@users.noreply.github.com>
2020-10-09 14:51:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 def index():
     a=os.environ['Authorization']
     try:
-       f = open("student.csv", "r")
-       for line in f.readlines():
-           print(line)
-           a = line.split(",")
-           if(a[0]=="00001"):
+        f = open("student.csv", "r")
+        for line in f.readlines():
+            print(line)
+            a = line.split(",")
+            if(a[0]=="00001"):
                 return a[4]
         f.close()
     except Exception:
